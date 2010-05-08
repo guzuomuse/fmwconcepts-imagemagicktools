975406dde47bf9bc3d148ba35dae261b8b6cc442
Anton P. Linevich <chexov@unixmaniacs.com>
2010-05-08 04:31:29 +0300

    putting all the scripts into ./bin folder

--- fmwconcepts-update.sh
+++ fmwconcepts-update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -uex
 cd $(dirname $0)
+cd ./bin/
 wget 'http://www.fmwconcepts.com/imagemagick/script_list.txt' -O script_list.txt
 awk -F\-\- '{ print $1}' script_list.txt | egrep '^\w' | while read name; do
     wget "http://www.fmwconcepts.com/imagemagick/downloadcounter.php?scriptname=${name}&dirname=${name}" -O ${name}
